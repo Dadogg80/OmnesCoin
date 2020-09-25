2506368e66b594bbcc03c5fdcc8cb63771737aaf
anditech <71329092+anditech@users.noreply.github.com>
2020-09-25 13:47:33 +0200

    Update
    Updated logic.js with
    function for API response
    Async function to create dynamic tablewith a loop to itirate over JSON
    
    Updated style.css
    to have a sticky  black header for the dynamiuc table
    Updated index.htlm
    Created a div for the dynamic table

--- logic.js
+++ logic.js
@@ -1,49 +1,54 @@
-src="https://code.jquery.com/jquery-3.5.1.min.js"
 
-// This is the API link used to fetch all the data needed
-let marketCap = "https://api.coingecko.com/api/v3/global"
-let listOfCoins = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
+$(document).ready(function() {
 
+//Global object toLocaleString method to format the data into a currency string
+const euroObj = {
+        style: "currency",
+        currency: "EUR"
+        }
 
+//Created a funtction that get a API response from coingecko and return the response as a JSON file.
+function getData(){
+  // variable url is the url for the API we can to call
+  let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&per_page=100&page=0";
+  // return initiates/ call function fetch(url) and gets the response.json in return from the fetch(url) function call.
+  return fetch(url)
+  .then(res => {
+      return res.json();
+  })
+  .catch (err => {
+    console.log(err);
+  });
+  }
 
-fetch(marketCap)
-        .then( res => {
-            res.json().then( data => {
-                console.log(data);
-                console.log(data.data.active_cryptocurrencies);
+//Created Async function for creating the table of all coins.
+async function createTable () {
+// Created a variable coin that is binded to the response.json returned from the getData() function.
+    let coin = await getData();
 
+    for(let index = 0; index < 100; index ++){
 
-              })
-        });
-
-fetch(marketCap)
-        .then( res => {
-            res.json().then( data => {
-                console.log(data);
-                console.log(data.data.markets);
-
-
-              })
-        });
-
-fetch(marketCap)
-        .then( res => {
-            res.json().then( data => {
-                console.log(data);
-                console.log(data.data.markets);
-
-
-              })
-        });
-
-
-
-fetch(listOfCoins)
-        .then( res => {
-            res.json().then( data => {
-                console.log(data[0].current_price);
-                  let price = $("#bitcoin").html("BTC: " + data[0].current_price);
-                  return price;
-
-              })
-        });
+      //adding all information to the table with id "coinTable" in the html file.
+      $('#coinTable').append(
+        // inside the coinTable, add a new table row with the rank, name, marketcap, curren_price, total_volume, circulating_supply, and price change presentage for 24h of each coin.
+        $('<tr class="content-row table-dark"></tr>').append(
+          // insert the rank in the new row.
+          $('<td id = "rank"></td>').text(index+1),
+          // insert the name in the new row.
+          $('<td id = "name"></td>').text(coin[index].name),
+          // insert the market cap in the new row.
+          $('<td id = "marketCap"></td>').text((coin[index].market_cap).toLocaleString("en-GB", euroObj)),
+          // insert the current price in the new row.
+          $('<td id = "price"></td>').text((coin[index].current_price).toLocaleString("en-GB", euroObj)),
+          // insert the total volume in the new row.
+          $('<td id = "volume"></td>').text((coin[index].total_volume).toLocaleString("en-GB", euroObj)),
+          // insert the circulating supply in the new row.
+          $('<td id = "supply"></td>').text((coin[index].circulating_supply).toLocaleString(2)),
+          // insert the 24 hopur change in the new row.
+          $('<td id = "change24"></td>').text((coin[index].price_change_percentage_24h).toFixed(2) + " " + "%"),
+        )
+)
+}
+}
+createTable();
+})
